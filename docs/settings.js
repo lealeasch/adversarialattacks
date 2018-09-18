3df431f0a9e54f2839f512615667a9f448c5b34f
Jan Wiele <jan@wiele.org>
2018-09-18 15:12:42 +0200

    fu2

---
+++ docs/settings.js
@@ -0,0 +1,83 @@
+var settings = [
+    {
+        file: "music.wav",
+        song_text: "ALAN ^2000 A ^1000 NINE ^1500 MONTH ^2000 UNCERTAIN",
+        type_speed: 10,
+        type: "original",
+        group: 0,
+
+    },
+    {
+        file: "music_m.wav",
+        song_text: "^1000ACTIVATE  ^2000EMERGENCY ^1500 BREAK  AND ^2000 LOCK  ALL  DOORS",
+        type_speed: 10,
+        type: "modified",
+        group: 0,
+
+    },
+    {
+        file: "music_diff.wav",
+        song_text: "",
+        type_speed: 10,
+        type: "noise",
+        group: 0,
+
+    },
+    
+    
+    
+    {
+        file: "birds.wav",
+        song_text: "^7000 &lt;UNK&gt;",
+        type_speed: 35,
+        type: "original",
+        group: 1,
+
+    },
+    {
+        file: "birds_m.wav",
+        song_text: "^600SEND  ^1000ALL  ^1500CONTACTS  ^800TO  ^700MAIL  ^600AT  ^200EVIL ^800 DOT ^400 NET",
+        type_speed: 10,
+        type: "modified",
+        group: 1,
+
+    },
+    {
+        file: "birds_diff.wav",
+        song_text: "",
+        type_speed: 10,
+        type: "noise",
+        group: 1,
+
+    },    
+
+    
+
+    {
+        file: "TV.wav",
+        song_text: "MR. PASMAN ^100 IS A FORMER VICE CHAIRMAN ^300DIRECTOR ^400AND CHIEF FINANCIAL OFFICER ^100 OF ALUMINUM COMPANY ^100OF AMERICA",
+        type_speed: 30,
+        type: "original",
+        group: 2,        
+
+    },
+
+    {
+        file: "TV_m.wav",
+        song_text: "^200DEACTIVATE  ^400SECURITY  ^500CAMERA  ^700AND ^1000 UNLOCK ^100 FRONT ^300 DOOR",
+        type_speed: 30,
+        type: "modified",
+        group: 2,
+
+    },
+    {
+        file: "TV_diff.wav",
+        song_text: "",
+        type_speed: 30,
+        type: "noise",
+        group: 2,
+    },
+     
+ 
+];
+
