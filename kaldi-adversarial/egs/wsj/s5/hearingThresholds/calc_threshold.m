1910a9715f36f7222a589a6849e28a26108fc411
LdyXOR <lea.schoenherr@rub.de>
2019-03-22 10:11:06 +0100

    added example data in kaldi-adversarial/egs/wsj/s5/data/music and kaldi-adversarial/egs/wsj/s5/music

--- kaldi-adversarial/egs/wsj/s5/hearingThresholds/calc_threshold.m
+++ kaldi-adversarial/egs/wsj/s5/hearingThresholds/calc_threshold.m
@@ -1,12 +1,6 @@
 function remapped_heating_thr = calc_threshold(wav_files, win_len, overlap, destination)
 
-%add_pre = '/media/kaldi-monster';
 add_pre = '';
-
-% destination = strcat(add_pre, '/home/lea/github/adversarialattacks/kaldi-adversarial/egs/wsj/s5/exp/nnet5d_gpu_time/spoof_test_dev93_mt_9/thresholds/');
-% wav_files = strcat(add_pre, '/home/lea/github/adversarialattacks/kaldi-adversarial/egs/wsj/s5/data/test_dev93_mt_9/wav.scp');
-% win_len = 512;
-% overlap = 256;
 
 addpath('MPEG1')
 
@@ -22,11 +16,16 @@ while ischar(tline)
 end
 fclose(fid);
 
+if ~exist(destination, 'dir')
+  mkdir(destination);
+end
+
 for uu = 1:length(utt)
     
     wav_file = utt{uu};
     
-    [audio, fs] = audioread(strcat(add_pre, wav_file));
+    display(strcat('Calculates Hearing Thresholds for: ', add_pre, wav_file))
+    [audio, fs] = audioread(strcat('../', add_pre, wav_file));
     [remapped_heating_thr, remapped_heating_thr_dB] = calculate_hearing_threshold(audio, fs, win_len, overlap);
       
     % repeat first and last frame for padding
